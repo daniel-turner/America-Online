ce679f08bc12f425f71e625178e76faf37e0e30b
danturne <danturne@yahoo.com>
2015-07-03 19:05:14 -1000

    complete through kicking

--- public/js/client.js
+++ public/js/client.js
@@ -1,13 +1,29 @@
 (function() {
-  var SERVER_ADDRESS = 'http://localhost:8000';
-  var SOCKET_CONNECT = 'connect';
-  var SOCKET_DISCONNECT = 'disconnect';
-  var SOCKET_RECONNECTING = 'reconnecting';
-  var SOCKET_RECONNECT = 'reconnect';
-  var SOCKET_ERROR = 'error';
-  var SOCKET_USER_MESSAGE = 'user message';
-  var SYSTEM = 'System';
+  var SERVER_ADDRESS = "http://localhost:8000";
+  var SOCKET_CONNECT = "connect";
+  var SOCKET_DISCONNECT = "disconnect";
+  var SOCKET_RECONNECTING = "reconnecting";
+  var SOCKET_RECONNECT = "reconnect";
+  var SOCKET_ERROR = "error";
+  var SOCKET_USER_MESSAGE = "user message";
+  var SOCKET_USER_MENTION = "mention";
+  var SOCKET_USER_KICK = "kick";
+  var SYSTEM = "System";
+  var SERVER_USER = "admin";
   var SOCKET_USER_REGISTRATION = "user registration";
+  var SOCKET_USERLIST_UPDATE = "userlist update";
+  var myNickname = "";
+
+  //manage state
+  //show registration
+  //default state
+  var registration = $('#registration');
+  var chatroom = $('#chatroom');
+
+  //hide chatroom
+  chatroom.hide();
+  // connectedUsersContainer.hide();
+
   var socket = io.connect(SERVER_ADDRESS);
 
   socket.on(SOCKET_CONNECT, function() {
@@ -34,31 +50,75 @@
     }
   });
 
+  socket.on(SOCKET_USER_KICK, function(reason) {
+
+    message(SERVER_USER, "Contemplate your sin: " + reason.reason);
+  });
+
   socket.on(SOCKET_USER_MESSAGE, function(from, userMessage) {
 
-    message('anonymous', userMessage);
+    message(from, userMessage);
+  });
+
+  socket.on(SOCKET_USERLIST_UPDATE, function(userList) {
+
+    connectedUsersUpdate(userList);
+
   });
 
   function message(from, message) {
 
-    var newMessage = $('<p>');
-    var fromTag = $('<b>', {
-      html:from
+    from = moment().format("h:mm:ss a") + " : " + from + " : ";
+
+    var formattedNickname = $("<span>", {
+
+      text: myNickname,
+      class: "mention"
     });
+
+    var mentionedMessage = message.replace(myNickname, formattedNickname.get(0).outerHTML);
+    mentionedMessage = mentionedMessage.replace(myNickname.toLowerCase(), formattedNickname.get(0).outerHTML);
+
+    var fromTag = $('<b>', {
+        html:from
+    });
+    var newMessage = $('<p>');
+
+    if(message !== mentionedMessage) {
+
+      fromTag.addClass("fromMention");
+
+    }
+
     var messageTag = $('<span>', {
-      html:message
+      html:mentionedMessage
     });
 
     newMessage.append(fromTag);
     newMessage.append(messageTag);
+
     $('#chatlog').append(newMessage).get(0).scrollTop = 100000000;
   }
+
+  function connectedUsersUpdate(connectedUsers) {
+
+    var list = $("#userList");
+    list.empty();
+    var listItem;
+
+    connectedUsers.forEach(function(user) {
+
+      listItem = document.createElement("li");
+      listItem.innerHTML = user;
+      list.append(listItem);
+    })
+  };
 
   $('#messageForm').submit(function() {
     var messageField = $('#message');
     var theMessage = messageField.val();
     //add message to chatlog
-    message('me',theMessage);
+    message(myNickname,theMessage);
     //send message to server
     socket.emit(SOCKET_USER_MESSAGE, theMessage);
     //clear message input field
@@ -73,37 +133,26 @@
     //send nickname to server
     socket.emit(SOCKET_USER_REGISTRATION, nickname, function(available){
 
-      //   console.log('available', available);
-      // });
       //if nickname is available
       // goto chatroom
       if(available) {
 
         changeStateToChatRoom();
+        myNickname = nickname;
 
-      } else {
+      } else {// else show error
 
         $('#nickname_error').text('Nickname is taken!');
       }
-      // else show error
-
 
     });
     return false;
   });
 
-  //manage state
-  var registration = $('#registration');
-  var chatroom = $('#chatroom');
-
-  //default state
-  //show registration
-  //hide chatroom
-  chatroom.hide();
-
   function changeStateToChatRoom() {
 
     chatroom.show();
+    // connectedUsersContainer.show();
     registration.hide();
   };
 })();
--- server.js
+++ server.js
@@ -1,39 +1,129 @@
 var PORT = 8000;
-var SOCKET_CONNECTION = 'connection';
-var SOCKET_USER_MESSAGE = 'user message';
-var SOCKET_USER_REGISTRATION = 'user registration';
-var SERVER_USER = 'The Server';
-var socketIO = require('socket.io');
-//listening for socket connections on port 8000
-var server = socketIO.listen(PORT);
+var SOCKET_CONNECTION = "connection";
+var SOCKET_USER_MESSAGE = "user message";
+var SOCKET_USER_REGISTRATION = "user registration";
+var SOCKET_USERLIST_UPDATE = "userlist update";
+var SOCKET_USER_KICK = "kick";
+var SERVER_USER = "admin";
+var socketIO = require("socket.io");
+var moment = require("moment");
 var nicknames = {};
 
+function processServerCommands(chunk) {
+
+  var namespace = server.of("/");
+  process.stdout.setEncoding('utf8');
+
+  // console.log(namespace.connected);
+
+  var connections = namespace.connected;
+
+  var commandInput = chunk.substring(0,chunk.length-1).split(" ");
+
+  if(commandInput.length < 1) {
+
+    process.stdout.write("Could not process admin input");
+    return;
+  }
+
+  switch (commandInput[0]) {
+
+    case "/kick":
+
+      if(commandInput.length < 2) {
+
+        process.stdout.write("Must specify a user to kick");
+        return;
+      }
+
+      if(!nicknames.hasOwnProperty(commandInput[1])) {
+
+        process.stdout.write("Unrecognized user");
+        return;
+      }
+
+      for(var id in connections) {
+
+        if(connections[id].nickname === commandInput[1]) {
+
+          var reason = null;
+
+          if(commandInput.length > 2) {
+
+            commandInput.splice(0,2);
+
+            reason = commandInput.join(" ");
+          }
+
+          connections[id].emit(SOCKET_USER_KICK, {
+
+            reason:reason
+          });
+
+          server.emit(SOCKET_USER_MESSAGE,
+                                SERVER_USER,
+                                commandInput[1] + " is being kicked.");
+
+          var ip = connections[id].handshake.address;
+          var nickname = connections[id].nickname;
+
+          connections[id].disconnect(true);
+
+          process.stdout.write(nickname + " at " + ip + " has been kicked.");
+
+          return;
+        }
+      }
+
+      process.stdout.write("Could not find user to kick");
+      break;
+
+    default:
+
+      process.stdout.write("Unrecognized command");
+      break;
+  };
+};
+
+//listening for socket connections on port 8000
+var server = socketIO.listen(PORT);
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(chunk) {
+
+  processServerCommands(chunk);
+});
 
 //handle connection events
 server.sockets.on(SOCKET_CONNECTION, function(socket) {
 
   socket.on(SOCKET_USER_MESSAGE, function(message) {
 
+    // var timeStamp = moment().format("h:mm:ss a");
+
     //broadcast this message to all connections
     socket.broadcast.emit(SOCKET_USER_MESSAGE, socket.nickname, message);
   });
 
   socket.on(SOCKET_USER_REGISTRATION, function(nickname,callback) {
-    if(nicknames.hasOwnProperty(nickname)) {
 
-      //not available
+    if(nicknames.hasOwnProperty(nickname)) { //not available
 
       callback(false);
 
-    } else {
+    } else { //available
 
       nicknames[nickname] = nickname;
+      var nicknameList = Object.keys(nicknames).sort();
+      console.log(nicknameList);
 
       socket.nickname = nickname;
 
       socket.broadcast.emit(SOCKET_USER_MESSAGE, SERVER_USER, nickname + " has connected");
-
+      server.emit(SOCKET_USERLIST_UPDATE, nicknameList);
       callback(true);
     }
   });
 });
+
+
